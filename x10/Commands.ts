4b2277205eafa5afea7539630e2104a2f6d78474
Morgan Thomas <m@m0rg.dev>
2020-09-17 21:56:58 -0700

    refactor x10 install step

--- x10/Commands.ts
+++ x10/Commands.ts
@@ -1,5 +1,6 @@
 import * as child_process from 'child_process';
 import * as path from 'path';
+import * as fs from 'fs';
 
 import { Config } from './Config';
 import { Repository } from './Repository';
@@ -26,7 +27,8 @@ export class Commands {
             already_found.add(depend);
             if (!desc_cache.get(depend)) desc_cache.set(depend, await repo.getPackageDescription(depend));
             const depend_desc = desc_cache.get(depend);
-            if (depend_desc.version.compare(hostdb.getInstalledVersion(depend)) == 0) {
+            if (depend_desc.version.compare(hostdb.getInstalledVersion(depend)) == 0
+                && await repo.buildExists(depend)) {
                 return false;
             }
             return true;
@@ -57,7 +59,7 @@ export class Commands {
         return Array.from(depends_final.values());
     }
 
-    static async buildPackages(package_names: string[]): Promise<boolean> {
+    private static async runWithContainer(callback: (container_id: string, mountpoint: string) => Promise<boolean>): Promise<boolean> {
         let rc = true;
         const buildah_from = child_process.spawn('buildah', ['from', Config.build_container]);
         let container_id: string = "";
@@ -67,10 +69,6 @@ export class Commands {
             buildah_from.on('exit', () => res());
             buildah_from.on('error', () => rej());
         });
-
-        const repo = new Repository(Config.repository);
-        interface step { atom: Atom, depends: Set<Atom> };
-        let plan: step[] = [];
 
         await buildah_from_p;
         container_id = container_id.trim();
@@ -84,13 +82,34 @@ export class Commands {
             const mountpoint = mount_rc.stdout.toString().trim();
             console.log(`Container root at ${mountpoint}`);
 
+            rc = await callback(container_id, mountpoint);
+        } catch (e) {
+            console.error(`Got error: ${e}`);
+            console.error(e);
+            rc = false;
+        } finally {
+            console.log("Cleaning up...");
+            if (container_mounted) child_process.spawnSync('buildah', ['umount', container_id], { stdio: 'inherit' });
+            child_process.spawnSync('buildah', ['rm', container_id], { stdio: 'inherit' });
+        }
+        return rc;
+    }
+
+    static async buildPackages(package_names: string[]): Promise<boolean> {
+        let rc = true;
+
+        const repo = new Repository(Config.repository);
+        interface step { atom: Atom, depends: Set<Atom> };
+        let plan: step[] = [];
+
+        rc = await Commands.runWithContainer(async (container_id, mountpoint) => {
             const hostdb = (Config.without_hostdb) ? Database.empty() : Database.construct(path.join(mountpoint, 'var/lib/x10/database/'));
 
             const atoms = await Promise.all(package_names.map((p) => AtomUtils.resolveUsingRepository(p, repo)));
 
-            let resolved: Set<Atom> = new Set();
-            let remaining: Set<Atom> = new Set();
-            let descs: Map<Atom, PackageDescription> = new Map();
+            const resolved: Set<Atom> = new Set();
+            const remaining: Set<Atom> = new Set();
+            const descs: Map<Atom, PackageDescription> = new Map();
 
             for (const atom of atoms) {
                 remaining.add(atom);
@@ -191,15 +210,8 @@ export class Commands {
             for (const step of plan) {
                 console.log(`${step.atom}\x1b[30G${Array.from(step.depends.values()).join(' ')}`);
             }
-        } catch (e) {
-            console.error(`Got error: ${e}`);
-            console.error(e);
-            rc = false;
-        } finally {
-            console.log("Cleaning up...");
-            if (container_mounted) child_process.spawnSync('buildah', ['umount', container_id], { stdio: 'inherit' });
-            child_process.spawnSync('buildah', ['rm', container_id], { stdio: 'inherit' });
-        }
+            return true;
+        });
 
         if (rc) {
             for (const step of plan) {
@@ -217,44 +229,194 @@ export class Commands {
     }
 
     static async buildSingle(atom: Atom, host_install: Set<Atom>): Promise<boolean> {
-        let rc = true;
-        const buildah_from = child_process.spawn('buildah', ['from', Config.build_container]);
-        let container_id: string = "";
-        buildah_from.stdout.on('data', (data) => container_id += data.toString('utf8'));
-
-        const buildah_from_p = new Promise((res, rej) => {
-            buildah_from.on('exit', () => res());
-            buildah_from.on('error', () => rej());
-        });
-
         const repo = new Repository(Config.repository);
-        await buildah_from_p;
-        container_id = container_id.trim();
-        console.log(`Have working container: ${container_id}`);
-        let container_mounted = false;
-        try {
-            const mount_rc = child_process.spawnSync('buildah', ['mount', container_id]);
-            if (mount_rc.signal) throw `mount killed by signal ${mount_rc.signal}`;
-            if (mount_rc.status != 0) throw `mount exited with failure status`;
-            container_mounted = true;
-            const mountpoint = mount_rc.stdout.toString().trim();
-            console.log(`Container root at ${mountpoint}`);
-
+        return Commands.runWithContainer(async (container_id, mountpoint) => {
             const hostdb = (Config.without_hostdb) ? Database.empty() : Database.construct(path.join(mountpoint, 'var/lib/x10/database/'));
-            await repo.installPackages(host_install, hostdb, mountpoint);
-            
+            await Commands.installResolvedPackages(host_install, repo, hostdb, mountpoint);
+
             const pkgdesc: PackageDescription = await repo.getPackageDescription(atom);
             const ctx = new BuildContext(container_id, mountpoint);
             await ctx.build(atom, pkgdesc, repo);
-        } catch (e) {
-            console.error(`Got error: ${e}`);
-            console.error(e);
-            rc = false;
-        } finally {
-            console.log("Cleaning up...");
-            if (container_mounted) child_process.spawnSync('buildah', ['umount', container_id], { stdio: 'inherit' });
-            child_process.spawnSync('buildah', ['rm', container_id], { stdio: 'inherit' });
+            return true;
+        });
+    }
+
+    static async installPackages(atoms: Set<Atom>, repo: Repository, db: Database, target_root: string) {
+        const resolved: Set<Atom> = new Set();
+        const remaining: Set<Atom> = new Set(atoms);
+        const descs: Map<Atom, PackageDescription> = new Map();
+        const ordered_packages: Atom[] = [];
+
+        let last_remaining = new Set(remaining);
+        let iterations = 0;
+
+        while (remaining.size) {
+            for (const atom of remaining.values()) {
+                let desc = descs.get(atom);
+                if (!desc) descs.set(atom, desc = await repo.getPackageDescription(atom));
+
+                if (!desc.version.compare(db.getInstalledVersion(atom))) {
+                    resolved.add(atom);
+                    remaining.delete(atom);
+                    ordered_packages.push(atom);
+                } else if (desc.rdepend.every(a => resolved.has(a))) {
+                    resolved.add(atom);
+                    remaining.delete(atom);
+                    ordered_packages.push(atom);
+                } else {
+                    desc.rdepend.forEach((depend) => {
+                        if (!resolved.has(depend)) {
+                            remaining.add(depend);
+                        }
+                    });
+                }
+                process.stdout.write(`==> Cycle ${iterations} - ${resolved.size} resolved, ${remaining.size} unresolved\x1b[K\r`);
+            }
+            process.stdout.write("\n");
+            iterations++;
+            if (Array.from(last_remaining.values()).every((a) => remaining.has(a))) {
+                // We have a dependency cycle. Let's go ahead and pick some unlucky package to go first.
+                let target: Atom;
+                const candidates = Array.from(last_remaining.values());
+
+                // prefer virtual packages here
+                for (const candidate of candidates) {
+                    if (AtomUtils.getCategory(candidate) == 'virtual') {
+                        target = candidate;
+                        break;
+                    }
+                }
+                if (!target) {
+                    for (const candidate of candidates) {
+                        let desc = descs.get(candidate);
+                        if (!desc) descs.set(candidate, desc = await repo.getPackageDescription(candidate));
+                    }
+
+                    // prefer packages with fewer rdepends
+                    const candidates_sorted = candidates.sort((a, b) =>
+                        descs.get(a).rdepend.length - descs.get(b).rdepend.length
+                    );
+
+                    target = candidates_sorted[0];
+                }
+
+                let desc = descs.get(target);
+                if (!desc) descs.set(target, desc = await repo.getPackageDescription(target));
+
+                const depends_in: Atom[] = [];
+                const depends_out: string[] = [];
+
+                for (const depend of desc.rdepend) {
+                    if (resolved.has(depend)) {
+                        depends_in.push(depend);
+                    } else {
+                        depends_out.push(depend);
+                    }
+                }
+
+                if (!desc.version.compare(db.getInstalledVersion(target))) {
+                    //console.log(`==> Found existing build for ${target}`);
+                    // don't have to build this!
+                } else {
+                    if (depends_out.length) {
+                        console.log(`-!- Package ${target} will be installed before its ${depends_out.join(', ')} dependencies.`);
+                    }
+
+                }
+                resolved.add(target);
+                remaining.delete(target);
+                ordered_packages.push(target);
+            }
+            last_remaining = new Set(remaining);
         }
-        return rc;
+
+        const hooks_pending = new Set<string>();
+        for (const atom of ordered_packages) {
+            await Commands.installSingle(atom, repo, db, target_root, hooks_pending);
+        }
+
+        if (hooks_pending.has('ldconfig')) {
+            console.log("Running ldconfig...");
+            child_process.spawnSync('ldconfig', ['-X', '-r', target_root], { stdio: 'inherit' });
+        }
+    }
+
+    static async installResolvedPackages(atoms: Set<Atom>, repo: Repository, db: Database, target_root: string) {
+        const scratch_atoms = new Set(atoms);
+        const ordered_atoms: Atom[] = [];
+        const descs: Map<Atom, PackageDescription> = new Map();
+        const hooks_pending = new Set<string>();
+
+        while (scratch_atoms.size) {
+            for (const atom of scratch_atoms.values()) {
+                let desc = descs.get(atom);
+                if (!desc) descs.set(atom, desc = await repo.getPackageDescription(atom));
+
+                if (desc.rdepend.every((a) => !scratch_atoms.has(a))) {
+                    ordered_atoms.push(atom);
+                    scratch_atoms.delete(atom);
+                }
+            }
+        }
+
+        for (const atom of ordered_atoms) {
+            await Commands.installSingle(atom, repo, db, target_root, hooks_pending);
+        }
+
+        if (hooks_pending.has('ldconfig')) {
+            console.log("Running ldconfig...");
+            child_process.spawnSync('ldconfig', ['-X', '-r', target_root], { stdio: 'inherit' });
+        }
+    }
+
+    // TODO hooks stuff
+    static async installSingle(atom: Atom, repo: Repository, db: Database, target_root: string, hooks_pending: Set<string>) {
+        const pkgdesc: PackageDescription = await repo.getPackageDescription(atom);
+        console.log(`Installing ${atom} ${pkgdesc.version.version} to ${target_root}`);
+        db.install_pending(atom);
+        if (AtomUtils.getCategory(atom) != 'virtual') {
+            // TODO this should probably be a method on Repository
+            const build_path = await fs.promises.realpath(path.join(repo.local_builds_path, atom + "," + pkgdesc.version.version + ".tar.xz"));
+
+            const flist_rc = child_process.spawnSync('tar', ['xJf', build_path, `./var/lib/x10/database/${atom}.list`, '-O'],
+                { maxBuffer: 16 * 1024 * 1024 });
+            const file_list = JSON.parse(flist_rc.stdout.toString());
+
+            // running this in a transaction makes me feel real cool about it
+            // (come back to this!)
+            db.transaction(() => {
+                for (const file of file_list) {
+                    db.add_file(atom, file.name, file.type);
+                }
+            });
+
+            const rc = child_process.spawnSync('tar', ['xpJvf', build_path], {
+                cwd: target_root,
+                stdio: ['ignore', 'pipe', 'inherit']
+            });
+            if (rc.signal) throw `unpack killed by signal ${rc.signal}`;
+            if (rc.status != 0) throw `unpack exited with failure status`;
+
+            // begin file-based hooks
+
+            for (const file of rc.stdout.toString().split('\n')) {
+                if (file.endsWith('.so')) {
+                    hooks_pending.add('ldconfig');
+                }
+            }
+
+            // end file-based hooks
+        }
+        if (pkgdesc.post_unpack_script) {
+            // in case the post-unpack script needs it
+            if (hooks_pending.has('ldconfig')) {
+                console.log("Running ldconfig...");
+                child_process.spawnSync('ldconfig', ['-X', '-r', target_root], { stdio: 'inherit' });
+                hooks_pending.delete('ldconfig');
+            }
+            console.log(`Running post-unpack script for ${atom}`);
+            child_process.spawnSync('chroot', ['.', 'bash', '-c', pkgdesc.post_unpack_script], { stdio: 'inherit', cwd: target_root });
+        }
+        db.install(atom, pkgdesc.version);
     }
 };
--- x10/x10.ts
+++ x10/x10.ts
@@ -22,8 +22,8 @@ Config.use_default_depends = !argv.without_default_depends;
 Config.without_hostdb = argv.without_hostdb;
 Config.verbose_output = argv.verbose;
 
-if(argv.build_container) Config.build_container = argv.build_container;
-if(argv.repository) Config.repository = argv.repository;
+if (argv.build_container) Config.build_container = argv.build_container;
+if (argv.repository) Config.repository = argv.repository;
 
 function ensure_unshared(argv: any, callback: Function) {
     if (argv.unshared) {
@@ -35,51 +35,29 @@ function ensure_unshared(argv: any, callback: Function) {
 }
 
 async function main() {
-    if(argv.repository) argv.repository = path.resolve(argv.repository);
+    if (argv.repository) argv.repository = path.resolve(argv.repository);
 
     if (argv._.length > 0) {
         if (argv._[0] == 'install') {
             const repo = new Repository(argv.repository || '/var/lib/x10/repo', argv.remote_url);
-            const hostdb = (argv.without_hostdb) ? Database.empty() : Database.construct((argv.host_root || '') + '/var/lib/x10/database/');
             const targetdb = Database.construct((argv.target_root || '') + '/var/lib/x10/database/');
-            const tx = new Transaction(repo, hostdb, targetdb);
+            const to_install = await Promise.all(argv._.slice(1).map(async (shortpkg) => await AtomUtils.resolveUsingRepository(shortpkg, repo)));
 
-            await Promise.all(argv._.slice(1).map(async function (shortpkg) {
-                const resolved = await AtomUtils.resolveUsingRepository(shortpkg, repo);
-                await tx.addToTransaction(resolved, Location.Target);
-            }));
-            const plan = await tx.plan();
-            Transaction.displayPlan(plan);
+            await Commands.installPackages(new Set(to_install),
+                repo, targetdb, argv.target_root || '/');
 
-            const to_install = [];
-            
-            for (const step of plan) {
-                if (step.type == StepType.Build) {
-                    console.log("\nThis plan would require packages to be compiled from source.");
-                    process.exit(1);
-                } else if (step.type == StepType.TargetInstall) {
-                    to_install.push(step.what);
-                }
-            }
-
-            await repo.installPackages(new Set(to_install), targetdb, (argv.target_root || '/'));
-
-            await Promise.all(argv._.slice(1).map(async function (shortpkg) {
-                const resolved = await AtomUtils.resolveUsingRepository(shortpkg, repo);
-                targetdb.select(resolved);
-            }));
             targetdb.print_stats();
         } else if (argv._[0] == 'build') {
-           ensure_unshared(argv, async () => {
-               let success = await Commands.buildPackages(argv._.slice(1));
-               if(!success) process.exitCode = 1;
-           });
+            ensure_unshared(argv, async () => {
+                let success = await Commands.buildPackages(argv._.slice(1));
+                if (!success) process.exitCode = 1;
+            });
         } else if (argv._[0] == '_build_single') {
             ensure_unshared(argv, async () => {
                 const repo = new Repository(Config.repository);
                 const success = await Commands.buildSingle(await AtomUtils.resolveUsingRepository(argv._[1], repo),
                     new Set(await Promise.all(argv._.slice(2).map((p) => AtomUtils.resolveUsingRepository(p, repo)))));
-                if(!success) process.exitCode = 1;
+                if (!success) process.exitCode = 1;
             })
         } else if (argv._[0] == 'update') {
             const repo = new Repository(argv.repository || '/var/lib/x10/repo', argv.remote_url);
@@ -87,27 +65,11 @@ async function main() {
 
             const manifest = await repo.maybeUpdateManifest();
             for (const pkg of manifest.getAllPackages()) {
-                if(db.getInstalledVersion(pkg.atom) && pkg.version.compare(db.getInstalledVersion(pkg.atom))) {
+                if (db.getInstalledVersion(pkg.atom) && pkg.version.compare(db.getInstalledVersion(pkg.atom))) {
                     console.log(`Found outdated package: ${pkg.atom}`);
                 }
             }
 
-        } else if (argv._[0] == '_get_builds_for') {
-            const repo = new Repository(argv.repository || '/var/lib/x10/repo');
-            const hostdb = Database.construct((argv.host_root || '') + '/var/lib/x10/database/');
-            const targetdb = Database.construct((argv.target_root || '') + '/var/lib/x10/database/');
-            const tx = new Transaction(repo, hostdb, targetdb);
-
-            await Promise.all(argv._.slice(1).map(async function (shortpkg) {
-                const resolved = await AtomUtils.resolveUsingRepository(shortpkg, repo);
-                await tx.addToTransaction(resolved, Location.Target);
-            }));
-            const plan = await tx.plan();
-            for (const step of plan) {
-                if (step.type == StepType.Build) {
-                    console.log(step.what);
-                }
-            }
         }
     } else {
         print_help();
