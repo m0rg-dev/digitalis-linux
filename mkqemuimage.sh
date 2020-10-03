afa1d8734efd5a1656ab03bbf3ee01a1bc810b02
Morgan Thomas <m@m0rg.dev>
2020-10-03 14:09:36 -0700

    add script to build a QEMU image

--- mkqemuimage.sh
+++ mkqemuimage.sh
@@ -20,16 +20,46 @@ buildah unshare sh -exc '
     cp -r x10/* $dir/usr/share/x10
     cp -r repository $dir/var/lib/x10/repo
 
-    buildah run "$ctr" node /usr/share/x10/x10.js install kernel/linux
+    buildah run --net host "$ctr" node /usr/share/x10/x10.js install kernel/linux core/eudev
 
     ls $dir
     rm -r $dir/var/lib/x10/repo
-    sh -c "cd $dir; tar cp ." | xz -T 0 > rootfs.tar.xz
+    virt-make-fs --partition=mbr -t ext4 -s +10G --format=qcow2 -- $dir digitalis_rootfs.img
+    cp -r $dir/boot .
     buildah umount $ctr
 
     buildah rm $ctr
 '
 
-ctr=$(buildah from scratch)
-buildah add "$ctr" rootfs.tar.xz
-buildah commit "$ctr" digitalis
+cat > /tmp/99-auto-grub.start <<EOF
+set -x
+mkinitcpio -P
+grub-install /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+shutdown -p now
+EOF
+
+guestfish <<EOF
+add digitalis_rootfs.img
+run
+mount /dev/sda1 /
+copy-in /tmp/99-auto-grub.start /etc/local.d/
+chmod 0777 /etc/local.d/99-auto-grub.start
+mv /etc/runlevels/default/agetty.tty1 /
+write /etc/conf.d/local "rc_verbose=yes"
+EOF
+
+rm /tmp/99-auto-grub.start
+
+qemu-system-x86_64 -kernel boot/vmlinuz-* -initrd boot/initramfs-*-huge.img -hda digitalis_rootfs.img -curses -append "root=/dev/sda1" -m 4G -accel kvm
+
+guestfish <<EOF
+add digitalis_rootfs.img
+run
+mount /dev/sda1 /
+rm /etc/local.d/99-auto-grub.start
+rm /etc/conf.d/local
+mv /agetty.tty1 /etc/runlevels/default/
+EOF
+
+rm -r boot
