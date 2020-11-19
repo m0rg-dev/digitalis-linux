6173423d49724f4fa5fe47b57a4f4d8f506e83c8
Morgan Thomas <m@m0rg.dev>
2020-11-19 13:14:12 -0800

    wrap log lines better

--- x10/LogInfo.tsx
+++ x10/LogInfo.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ink from 'ink';
 import { LogLine } from "./Logger";
+import useStdoutDimensions from 'ink-use-stdout-dimensions';
 
 export type LogLines = {
     rels: Map<string, string>,
@@ -11,8 +12,9 @@ export class LogStatic extends React.Component<{ progress: LogLines }> {
     render() {
         return <ink.Static items={this.props.progress.lines.filter(line => line.type == "message")}>
             {line => {
-                return <ink.Box key={line.uuid}>
-                    <ink.Text color="gray">{line.context.name}</ink.Text><ink.Text> {line.message}</ink.Text>
+                return <ink.Box key={line.uuid} width={Math.min(process.stdout.getWindowSize()[0], 250)}>
+                    <ink.Box minWidth={line.context.name.length} height={1}><ink.Text color="gray">{line.context.name}</ink.Text></ink.Box>
+                    <ink.Box paddingLeft={1}><ink.Text>{line.message}</ink.Text></ink.Box>
                 </ink.Box>
             }}
         </ink.Static>;
